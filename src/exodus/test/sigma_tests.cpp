ade95908b34b4ebaf5db5dc933939a95e491e576
thebevrishot <panu.suk@satang.com>
2019-08-16 20:02:51 +0700

    Add full group test

--- src/exodus/test/sigma_tests.cpp
+++ src/exodus/test/sigma_tests.cpp
@@ -1,10 +1,11 @@
 #include "../sigma.h"
+#include "../../test/test_bitcoin.h"
 
 #include <boost/test/unit_test.hpp>
 
 #include <vector>
 
-BOOST_AUTO_TEST_SUITE(exodus_sigma_tests)
+BOOST_FIXTURE_TEST_SUITE(exodus_sigma_tests, BasicTestingSetup)
 
 BOOST_AUTO_TEST_CASE(private_key)
 {
@@ -61,6 +62,28 @@ BOOST_AUTO_TEST_CASE(proof)
 
     BOOST_CHECK_EQUAL(proof.Verify(sigma::Params::get_default(), pubs.begin(), pubs.end()), true);
     BOOST_CHECK_EQUAL(proof.Verify(sigma::Params::get_default(), pubs.begin(), pubs.end() - 1), false);
+}
+
+#define SIGMA_MAX_GROUP_SIZE 16384
+
+BOOST_AUTO_TEST_CASE(fullgroup)
+{
+    std::vector<exodus::SigmaPublicKey> pubs;
+    pubs.reserve(SIGMA_MAX_GROUP_SIZE + 1);
+    exodus::SigmaPrivateKey priv;
+    while (pubs.size() < SIGMA_MAX_GROUP_SIZE + 1) {
+        priv.Generate();
+        pubs.push_back(exodus::SigmaPublicKey(priv));
+    }
+
+    exodus::SigmaProof proof;
+    proof.Generate(priv, pubs.begin() + 1, pubs.end()); // use SIGMA_MAX_GROUP_SIZE coins
+    BOOST_CHECK(proof.Verify(sigma::Params::get_default(), pubs.begin() + 1, pubs.end()));
+
+    // exceed group size
+    exodus::SigmaProof invalidProof;
+    invalidProof.Generate(priv, pubs.begin(), pubs.end()); // use SIGMA_MAX_GROUP_SIZE + 1 coins
+    BOOST_CHECK(!invalidProof.Verify(sigma::Params::get_default(), pubs.begin(), pubs.end()));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
